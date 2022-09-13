203433724caee4c07c99fb490e5a4120c72efc58
Usuario <Usuario@B13-205-22953>
2022-09-13 11:50:01 -0500

    Agregar archivos de proyecto.

---
+++ Multiplicaciones/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicaciones
+{
+    public class Class1
+    {
+       public static long Multiplicacion (long i, long j)
+        {
+            return (i * j);
+        }
+    }
+}
